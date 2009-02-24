69a2c25b1d211127495c0371d332dab498ac02b6
ddared <ddared@gmail.com>
2009-02-24 16:40:05 +0300

    Rename

--- musicbox/lib/adapter.rb
+++
@@ -1,3 +0,0 @@
-class Adapter
-  
-end
--- musicbox/spec/musicboxadapter_spec.rb
+++ musicbox/spec/musicboxadapter_spec.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'spec'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/track')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/musicbox')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/musicboxadapter')
 
 
 describe Adapter do
