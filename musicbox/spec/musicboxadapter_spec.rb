4b12edc3f6d749cc1f875d1ab32b2097ff506ab1
dig <ddared@gmail.com>
2009-02-24 21:15:53 +0300

    Playing MPEG stream from Jimmy.mp3 ...

--- musicbox/spec/musicboxadapter_spec.rb
+++ musicbox/spec/musicboxadapter_spec.rb
@@ -8,10 +8,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/musicboxadapter')
 describe MusicboxAdapter do
   
   it "should play music" do
-    @song1 = Track.new({:name => "Never Coming Home", :author => 'Sting', :filename => '/Users/dig/Music/iTunes/iTunes Music/Miles Davis/Kind Of Blue/01 So What.m4a'})
+    @song1 = Track.new({:name => "Never Coming Home", :author => 'Sting', :filename => '~/sites/lern/musicbox/music/Jimmy.mp3'})
     @musicbox = MusicBox.new
     @musicbox.add(@song1)
     @musicbox.play(0)
   end
-  
+=begin
+  Directory: /Users/dig/sites/lern/musicbox/music/
+  Playing MPEG stream from Jimmy.mp3 ...
+  MPEG 1.0 layer III, 192 kbit/s, 44100 Hz joint-stereo
+=end
 end
