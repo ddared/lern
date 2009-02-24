99157a8a962b9d19a22bc29da0bd9e3283742c98
dig <ddared@gmail.com>
2009-02-24 16:16:59 +0300

    add: adapter

---
+++ musicbox/spec/adapter_spec.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'spec'
+require File.expand_path(File.dirname(__FILE__) + '/../lib/track')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/musicbox')
+
+
+describe Adapter do
+  
+  
+  
+end
